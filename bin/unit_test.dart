baeb971e70952693097c1019bcc4068501e29a43
osma58 <43674296+osma58@users.noreply.github.com>
2020-10-30 15:29:03 +0100

    Add files via upload

---
+++ bin/unit_test.dart
@@ -0,0 +1,5 @@
+import 'package:unit_test/unit_test.dart' as unit_test;
+
+void main(List<String> arguments) {
+  print('Hello world');
+}
---
+++ unit_test.dart
@@ -0,0 +1,60 @@
+// Opdracht 1
+bool boolCheck() {
+  return false;
+}
+
+String varCheck() {
+  var name = 'Dia';
+  return name;
+}
+
+int biggerThenZeroCheck() {
+  var price = 2;
+  return price;
+}
+
+int isZeroCheck() {
+  var price = 0;
+  return price;
+}
+
+// Wij zouden de boolean kunnen gebruiken om te checken of iemand zijn postcode zich bevind in het gebied waar wij bezorgen 0f
+// om the checken of ze betaald hebben of niet.
+
+// Ook zouden we het checken of de var wel een waarde heeft kunnen gebruiken voor als iemand een review wilt achterlaten zonder
+// 1 van de velden niet in te voeren.
+
+// Opdracht 2
+int biggerThenZeroCheck2() {
+  var price = 2;
+  return price;
+}
+
+int isZeroCheck2() {
+  var price = 0;
+  return price;
+}
+
+int rangeCheck() {
+  var postcode = 15;
+  return postcode;
+}
+
+// Opdracht 3
+List listCheck() {
+  var lst = new List(3);
+  lst[0] = 12;
+  lst[1] = 'Second price';
+  lst[2] = 11;
+  return lst;
+}
+
+List splitCheck() {
+  var worded = 'Am-I-splited?';
+
+  var splitted = worded.split('-');
+
+  return splitted;
+}
+
+// Een list zouden wij kunnen gebruiken om de gekozen producten in een array the zetten.
